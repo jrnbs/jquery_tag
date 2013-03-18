709fd880e5e5c11be3d3e6e81e8728b1ce7d2a2c
jrnbs <Jeroenbos90@gmail.com>
2013-03-18 17:11:19 +0100

    Update jquery_tag.rb

--- lib/jquery_tag.rb
+++ lib/jquery_tag.rb
@@ -6,7 +6,7 @@ module JqueryTag # :nodoc:
   ## @return [String] the current (mostly latest) version of jQuery.
   def self.version;    '1.7.0'; end
   ## @return [String] the current (mostly latest) version of jQuery UI.
-  def self.ui_version; '1.8.16'; end
+  def self.ui_version; '1.10.2'; end
 end
 
 ActionView::Base.send(:include, JqueryTag::RailsHelper) if defined? Rails
